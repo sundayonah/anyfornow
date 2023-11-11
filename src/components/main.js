df9752e50f65f67ff4fd9b5acd70e86b5a413c4e
sundayonah <sundayonah94@gmail.com>
2023-11-11 11:33:29 +0100

    updates

--- src/components/main.js
+++ src/components/main.js
@@ -61,12 +61,56 @@ const MainPage = () => {
                </div>
             </div>
          </div>
-         <div className="w-[80%]">
-            <div className="flex justify-center items-center py-7">
-               <button>Stake</button>
-               <button>unStake</button>
+         <div className="w-[80%] px-8">
+            <div className="flex justify-center items-center py-7 ">
+               <button className="bg-transparent border border-gray-600 px-1 p-2">
+                  Stake
+               </button>
+               <button className="bg-gray-500 border border-gray-600 px-1 p-2">
+                  unStake
+               </button>
             </div>
-            <div className=" border">RIGHT SIDE</div>
+            <div className=" border border-gray-500 rounded-md">
+               <div className="flex justify-between items-center px-4 py-5 ">
+                  <span>Stake</span>
+                  <span>X</span>
+               </div>
+               <div className="w-[90%] m-auto flex justify-center border border-gray-500 px-4 py-1">
+                  <img
+                     src="/stake.jpg"
+                     width={20}
+                     height={20}
+                     alt="image"
+                     className="rounded-full"
+                  />
+                  <input
+                     className="w-full bg-transparent focus:outline-none p-1"
+                     placeholder="0.0"
+                  />
+                  <button className="text-sm px-1 bg-gray-600 rounded-md">
+                     MAX
+                  </button>
+               </div>
+
+               <div className="flex  justify-center items-center   px-4 py-2">
+                  <button className="w-full bg-gray-600 p-1">Stake</button>
+               </div>
+
+               <div className="flex justify-between items-center py-2 px-4">
+                  <span className="text-sm text-gray-500">
+                     You will Recieve
+                  </span>
+                  <p className="text-sm">0 ANC</p>
+               </div>
+               <div className="flex justify-between items-center py-2 px-4">
+                  <span className="text-sm text-gray-500">Exchange Rate</span>
+                  <p className="text-sm">1 ANC = 1500 ANC</p>
+               </div>
+               <div className="flex justify-between items-center py-2 px-4">
+                  <span className="text-sm text-gray-500">Staking APR</span>
+                  <p className="text-sm">17%</p>
+               </div>
+            </div>
          </div>
       </main>
    );
