1751fc29285c87c05d60b3fe5a50c5a59bdbeb5b
sundayonah <sundayonah94@gmail.com>
2023-12-17 20:13:50 +0100

    fix: remove italic from staking

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -4,9 +4,7 @@ const TopBar = () => {
    return (
       <div className="flex justify-center items-center mt-28 z-0">
          <div>
-            <h1 className="flex justify-center text-lg italic ">
-               stake MonieBot
-            </h1>
+            <h1 className="flex justify-center text-lg ">Stake MonieBot</h1>
             <span className="text-sm text-gray-500">
                {/* Stake ANC and ANC while Staking */}
             </span>
